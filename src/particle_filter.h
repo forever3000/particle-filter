048095bf059ab4ecd1d068f589054b731063e5b8
Liam <liam.bowers@adp.com>
2017-07-30 18:20:28 -0400

    small change

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -68,7 +68,7 @@ class ParticleFilter {
    * @param observations Vector of landmark observations
    */
   void dataAssociation(std::vector<LandmarkObs> predicted,
-                       std::vector<LandmarkObs>& observations);
+                       std::vector<LandmarkObs> &observations);
 
   /**
    * updateWeights Updates the weights for each particle based on the likelihood
